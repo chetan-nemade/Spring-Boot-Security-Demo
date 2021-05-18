22c6516eb050263d369a5dca0a9d46f8e5ebd85b
Chetan Nemade <chetan.nemade@gmail.com>
2021-05-18 11:30:21 +0530

    first commit...

---
+++ HttpBasicAuth/src/main/java/in/security/HttpBasicAuthApplication.java
@@ -0,0 +1,13 @@
+package in.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpBasicAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpBasicAuthApplication.class, args);
+	}
+
+}
---
+++ HttpBasicAuth/src/main/java/in/security/rest/WelcomeRestController.java
@@ -0,0 +1,16 @@
+package in.security.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/security-app/v1")
+public class WelcomeRestController {
+
+		@GetMapping("/")
+		public String massege()
+		{
+			return "Welcome to Security app...";
+		}
+}
